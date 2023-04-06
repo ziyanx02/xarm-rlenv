e373222c928ae3d9db623006fa1c85a1c0c28a32
ziyanx02 <ziyanx02@Gmail.com>
2023-04-06 16:12:52 -0700

    2023-4-5 version

--- test.py
+++ test.py
@@ -2,7 +2,11 @@ import xarm_env
 import numpy as np
 import cv2
 
-env = xarm_env.make()
-image0 = env.reset()
-
-image1 = env.step(np.array([1.0, 1.0], dtype=np.float16))
+env = xarm_env.make("lift", obs_mode="visual", use_ViT=False)
+obs = env.reset()
+cv2.imwrite("initial.jpg", obs)
+obs, _, _, _ = env.step(np.array([1.0, 0, 0, 0], dtype=np.float16))
+obs, _, _, _ = env.step(np.array([0, 1.0, 0, 0], dtype=np.float16))
+obs, _, _, _ = env.step(np.array([0, 0, 1.0, 0], dtype=np.float16))
+obs, _, _, _ = env.step(np.array([0, 0, 0, 1.0], dtype=np.float16))
+cv2.imwrite("final.jpg", obs)
